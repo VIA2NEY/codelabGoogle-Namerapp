47821a231dff8ee7f0b41e0d2ae7a5a24fe0989c
VIA2NEY <kouameangeV11@gmail.com>
2024-05-20 16:42:30 +0000

    adding device preview 2

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bfd6b25403f24d90a3265e47875e91f3",
-"/": "bfd6b25403f24d90a3265e47875e91f3",
+"index.html": "5954ab8f941d0451080cc445311a85c6",
+"/": "5954ab8f941d0451080cc445311a85c6",
 "main.dart.js": "bdbf46f6c44c80a9a3140d1041b50b22",
 "manifest.json": "08bf9a8512ce11aff052b99e3ac32c1f",
 "version.json": "9578f4f585da00760d4d0dfbb978496e"};
